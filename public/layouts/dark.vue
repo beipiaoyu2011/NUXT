41c3e74f60817025a0ecb3b1118f40c6271416a7
wangzhao <wangzhao@go-goal.com>
2018-07-31 16:53:12 +0800

    dark.vue

---
+++ public/layouts/dark.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="dark">
+        <nuxt></nuxt>
+    </div>
+</template>
+
+
+<style>
+body {
+  overflow: hidden;
+}
+.dark {
+  width: 100%;
+  min-width: 100%;
+  background: #000;
+  color: white;
+  position: absolute;
+  bottom: 0;
+  top: 0;
+  text-align: center;
+  padding: 20px;
+}
+.dark a {
+  color: white;
+}
+</style>
--- public/pages/about.vue
+++ public/pages/about.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        about
+        <div>about from {{name}}</div>
         <nuxt-link to="/">Go to Index</nuxt-link>
     </div>
 </template>
+<script>
+export default {
+    layout: 'dark',
+    asyncData({ req }) {
+        return {
+            name: req ? 'server' : 'client'
+        }
+    }
+}
+</script>
